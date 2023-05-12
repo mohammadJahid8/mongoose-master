8363a382a4d3027691789a681e886132c49820f1
mohammadJahid8 <mohammadjahid0007@gmail.com>
2023-05-12 14:41:07 +0600

    c

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
-const express = require("express");
-const mongoose = require("mongoose");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
 const app = express();
 const port = 5000;
+
+app.use(cors());
 
 // db connection
 async function main() {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/test");
     console.log("Mongodb connected");
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
   } catch (err) {
     console.log(`Failed to connect db`, err);
   }
@@ -19,8 +26,4 @@ main();
 
 app.get("/", (req, res) => {
   res.send("Hello world");
-});
-
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
 });
